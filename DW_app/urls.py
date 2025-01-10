d2cd2ca950e17d7d9f435ab9d6bd6ccd02fe5db1
Helpless Owner <ashishkr9214jmp@gmail.com>
2025-01-10 17:50:39 +0530

    login issue solving

--- DW_app/urls.py
+++ DW_app/urls.py
@@ -6,14 +6,14 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('customers', CustomerViewSet, basename='customer_details')
-# router.register('login', LoginViewSet, basename="login")
+router.register('login', LoginViewSet, basename="login")
 router.register('register', CustomerViewSet, basename="register")
 router.register('clothes', ClothesViewSet, basename='clothes_list')
 router.register('order',OrderViewSet,basename="order")
 
 urlpatterns = [
     path('change/password/',changePassword, name='change-password' ),
-    path('login/', login_view, name="login"),
+    # path('login/', login_view, name="login"),
     path('send_otp/',send_otp, name="send-otp"),
     path('', include(router.urls)),
 ]
